e804bdca3287259f11045151a6bff9ec28512473
sisshmue <sisshmue404@gmail.com>
2024-12-09 16:22:12 +0700

    walkThrough dropdown modifications

--- lib/components/change_language_dropdown_button.dart
+++ lib/components/change_language_dropdown_button.dart
@@ -12,60 +12,60 @@ class DropDown extends StatefulWidget {
 class _DropDownState extends State<DropDown> {
   String? selectedValue = "English";
   Function(String)? whenChanged;
-  final List<Map<String, String>> items =[
-    {"name" : "English", "icon" : "icons/uk.png"},
-    {"name" : "MM", "icon" : "icons/myanmar.png"}
-  ] ;
+  final List<Map<String, String>> items = [
+    {"name": "English", "icon": "icons/uk.png"},
+    {"name": "မြန်မာ", "icon": "icons/myanmar.png"}
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-  
-      child: DropdownButtonHideUnderline(
-        child: DropdownButton2<String>(
-          iconStyleData: IconStyleData(
-            icon: Icon(Icons.keyboard_arrow_down),
-          ),
-          isExpanded: true,
-          items: items
-              .map((item) => DropdownMenuItem<String>(
-                    value: item["name"],
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          item["icon"]!,
-                          width: 16,
-                          height: 16,
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2<String>(
+        iconStyleData: const IconStyleData(
+          icon: Icon(Icons.keyboard_arrow_down),
+        ),
+        isExpanded: true,
+        items: items
+            .map((item) => DropdownMenuItem<String>(
+                  value: item["name"],
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        item["icon"]!,
+                        width: 16,
+                        height: 16,
+                      ),
+                      const SizedBox(width: 3),
+                      Text(
+                        item["name"]!,
+                        style: GoogleFonts.bricolageGrotesque(
+                          textStyle: const TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.w500),
                         ),
-                        const SizedBox(width: 3),
-                        Text(
-                          item["name"]!,
-                          style: GoogleFonts.bricolageGrotesque(textStyle:  TextStyle(fontSize: 12.5, fontWeight: FontWeight.bold),),
-                        ),
-                      ],
-                    ),
-                  ))
-              .toList(),
-          value: selectedValue,
-          onChanged: (String? value) {
-            setState(() {
-              selectedValue = value;
-            });
-          },
-          buttonStyleData: ButtonStyleData(
+                      ),
+                    ],
+                  ),
+                ))
+            .toList(),
+        value: selectedValue,
+        onChanged: (String? value) {
+          setState(() {
+            selectedValue = value;
+          });
+        },
+        buttonStyleData: ButtonStyleData(
             elevation: 10,
-            padding: EdgeInsets.symmetric(horizontal: 1),
-            height: 24,
-            width: 100,
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            height: 30,
+            width: 110,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(4),
-            )
-          ),
-          menuItemStyleData: const MenuItemStyleData(
-            height: 40,
-          ),
+            )),
+        menuItemStyleData: const MenuItemStyleData(
+          height: 40,
         ),
       ),
     );
-}}
+  }
+}
--- lib/screens/walkthrough.dart
+++ lib/screens/walkthrough.dart
@@ -10,7 +10,6 @@ class Walkthrough extends StatefulWidget {
   State<Walkthrough> createState() => _WalkthroughState();
 }
 
-var dropdownValue = 'English';
 final _controller = PageController();
 
 class _WalkthroughState extends State<Walkthrough> {
@@ -18,192 +17,190 @@ class _WalkthroughState extends State<Walkthrough> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-          children: [
-            // Background image
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/background.png'),
-                  fit: BoxFit.cover,
-                ),
+        children: [
+          // Background image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/background.png'),
+                fit: BoxFit.cover,
               ),
             ),
-            // Main content
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Dropdown at the top
-                Padding(
-                  padding: const EdgeInsets.only(top: 60, left: 25),
-                  child: Align(alignment: Alignment.topLeft,child: DropDown()),
-                    
-                  
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                // PageView with page indicator
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          // Main content
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Dropdown at the top
+              const Padding(
+                padding: EdgeInsets.only(top: 60, left: 25),
+                child: Align(alignment: Alignment.topLeft, child: DropDown()),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              // PageView with page indicator
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Welcome to',
+                        style: GoogleFonts.bricolageGrotesque(
+                          textStyle: const TextStyle(
+                            fontSize: 24.41,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Sabai Jobs',
+                        style: GoogleFonts.bricolageGrotesque(
+                          textStyle: const TextStyle(
+                            fontSize: 30.52,
+                            color: Color(0xffFF3997),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: PageView(
+                      controller: _controller,
                       children: [
-                        Text(
-                          'Welcome to',
-                          style: GoogleFonts.bricolageGrotesque(
-                            textStyle: const TextStyle(
-                              fontSize: 24.41,
-                              fontWeight: FontWeight.w500,
+                        Center(
+                          child: Text(
+                            'Finding a Job Made Easy',
+                            style: GoogleFonts.bricolageGrotesque(
+                              textStyle: const TextStyle(fontSize: 15.6),
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          width: 10,
+                        Center(
+                          child: Text(
+                            'Offering Safe, Hassle-Free Jobs',
+                            style: GoogleFonts.bricolageGrotesque(
+                              textStyle: const TextStyle(fontSize: 15.6),
+                            ),
+                          ),
                         ),
-                        Text(
-                          'Sabai Jobs',
-                          style: GoogleFonts.bricolageGrotesque(
-                            textStyle: const TextStyle(
-                              fontSize: 30.52,
-                              color: Color(0xffFF3997),
-                              fontWeight: FontWeight.bold,
+                        Center(
+                          child: Text(
+                            'Shielding You from Scammers',
+                            style: GoogleFonts.bricolageGrotesque(
+                              textStyle: const TextStyle(fontSize: 15.6),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            'Enjoy Job Hunting with Us',
+                            style: GoogleFonts.bricolageGrotesque(
+                              textStyle: const TextStyle(fontSize: 15.6),
                             ),
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 50,
-                      child: PageView(
-                        controller: _controller,
-                        children: [
-                          Center(
-                            child: Text(
-                              'Finding a Job Made Easy',
-                              style: GoogleFonts.bricolageGrotesque(
-                                textStyle: const TextStyle(fontSize: 15.6),
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              'Offering Safe, Hassle-Free Jobs',
-                              style: GoogleFonts.bricolageGrotesque(
-                                textStyle: const TextStyle(fontSize: 15.6),
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              'Shielding You from Scammers',
-                              style: GoogleFonts.bricolageGrotesque(
-                                textStyle: const TextStyle(fontSize: 15.6),
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: Text(
-                              'Enjoy Job Hunting with Us',
-                              style: GoogleFonts.bricolageGrotesque(
-                                textStyle: const TextStyle(fontSize: 15.6),
-                              ),
-                            ),
-                          ),
-                        ],
+                  ),
+                  SmoothPageIndicator(
+                    controller: _controller,
+                    count: 4,
+                    effect: const ExpandingDotsEffect(
+                      dotHeight: 4,
+                      dotWidth: 4,
+                      activeDotColor: Color(0xffFF3997),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'By registering, you accept our Terms and Conditions of \n Use and our Privacy Policy.',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.bricolageGrotesque(
+                      textStyle: const TextStyle(
+                        fontSize: 12.5,
+                        color: Color(0xff4C5258),
                       ),
                     ),
-                    SmoothPageIndicator(
-                      controller: _controller,
-                      count: 4,
-                      effect: const ExpandingDotsEffect(
-                        dotHeight: 4,
-                        dotWidth: 4,
-                        activeDotColor: Color(0xffFF3997),
-                      ),
+                  ),
+                  Container(
+                    width: 343,
+                    height: 42,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text(
-                      'By registering, you accept our Terms and Conditions of \n Use and our Privacy Policy.',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.bricolageGrotesque(
-                        textStyle: const TextStyle(
-                          fontSize: 12.5,
-                          color: Color(0xff4C5258),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xffFF3997),
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(8), // Set the border radius
                         ),
                       ),
-                    ),
-                    Container(
-                      width: 343,
-                      height: 42,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Color(0xffFF3997),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                8), // Set the border radius
-                          ),
-                        ),
-                        child: Text(
-                          'Sign Up',
-                          style: GoogleFonts.bricolageGrotesque(
-                            textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15.63,
-                            ),
+                      child: Text(
+                        'Sign Up',
+                        style: GoogleFonts.bricolageGrotesque(
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.63,
                           ),
                         ),
                       ),
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      width: 343,
-                      height: 42,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: const Color(0xffFF3997),
-                        ),
-                        borderRadius: BorderRadius.circular(8),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: 343,
+                    height: 42,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color(0xffFF3997),
                       ),
-                      child: TextButton(
-                        onPressed: () {},
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                                8), // Set the border radius
-                          ),
-                        ),
-                        child: Text(
-                          'Log In',
-                          style: GoogleFonts.bricolageGrotesque(
-                            textStyle: const TextStyle(
-                              color: Color(0xffFF3997),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15.63,
-                            ),
-                          ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(8), // Set the border radius
                         ),
                       ),
+                      child: Text(
+                        'Log In',
+                        style: GoogleFonts.bricolageGrotesque(
+                          textStyle: const TextStyle(
+                            color: Color(0xffFF3997),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15.63,
+                          ),
+                        ),
+                      ),
                     ),
-                    const SizedBox(
-                      height: 60,
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
