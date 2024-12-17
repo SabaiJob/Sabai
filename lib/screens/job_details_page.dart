b67e3909265747c67d60916f01806a076f5754a0
Han Min Thaw <hanminthaw15@gmail.com>
2024-12-17 23:54:33 +0700

    added job details page

---
+++ lib/screens/job_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JobDetailsPage extends StatelessWidget {
+  const JobDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
