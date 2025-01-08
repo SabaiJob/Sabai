c7991c839701236bb9aafd1b74a85683128761bd
sisshmue <sisshmue404@gmail.com>
2025-01-08 16:52:34 +0700

    adding page refresh in all page

--- lib/screens/homepage_menupages/all.dart
+++ lib/screens/homepage_menupages/all.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sabai_app/screens/job_details_page.dart';
-import 'package:sabai_app/services/language_provider.dart';
+import 'package:sabai_app/constants.dart';
 import 'package:sabai_app/services/job_provider.dart';
-
 import '../../components/work_card.dart';
 
 class All extends StatelessWidget {
@@ -15,15 +12,24 @@ class All extends StatelessWidget {
     var jobProvider = Provider.of<JobProvider>(context);
     return Container(
       color: const Color(0xffF7F7F7),
-      child: ListView.builder(
-        itemCount: jobProvider.allJobs.length, // Total number of items
-        itemBuilder: (context, index) {
-          final job = jobProvider.combineJobs[index];
-          return WorkCard(
-            job['jobTitle'],
-            job['isPartner'],
+      child: RefreshIndicator(
+        color: primaryPinkColor,
+        backgroundColor: const Color(0xffFFEBF6),
+        onRefresh: () async {
+          await Future.delayed(
+            const Duration(seconds: 1),
           );
         },
+        child: ListView.builder(
+          itemCount: jobProvider.allJobs.length, // Total number of items
+          itemBuilder: (context, index) {
+            final job = jobProvider.combineJobs[index];
+            return WorkCard(
+              job['jobTitle'],
+              job['isPartner'],
+            );
+          },
+        ),
       ),
     );
   }
