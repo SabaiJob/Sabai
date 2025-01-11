c29de6aa629a47be1c5fa4abc5e1b7d9d4ffe20a
sisshmue <sisshmue404@gmail.com>
2025-01-11 17:48:17 +0700

    half way to contribute posting page

--- lib/screens/contribution_pages/posting.dart
+++ lib/screens/contribution_pages/posting.dart
@@ -1,70 +1,133 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_link_previewer/flutter_link_previewer.dart';
 import 'package:sabai_app/constants.dart';
-import 'package:sabai_app/screens/bottom_navi_pages/job_listing_page.dart';
 import 'package:sabai_app/screens/navigation_homepage.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' show PreviewData;
 
-class Posting extends StatelessWidget {
+class Posting extends StatefulWidget {
   Posting({this.url, this.img, super.key});
 
   String? url = '';
   String? img = '';
 
   @override
+  State<Posting> createState() => _PostingState();
+}
+
+class _PostingState extends State<Posting> {
+  PreviewData? _previewData;
+  final textController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    final uri = Uri.parse(url!);
+    final uri = Uri.tryParse(widget.url!);
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: primaryPinkColor,
+        ),
         backgroundColor: backgroundColor,
         title: const Text(
           'Post',
           style: appBarTitleStyleEng,
         ),
         centerTitle: true,
-        iconTheme: const IconThemeData(
-          color: primaryPinkColor,
-        ),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 10.0),
-            child: TextButton(
-              style: TextButton.styleFrom(
-                  minimumSize: const Size(82, 29),
+            padding: const EdgeInsets.only(right: 15.0),
+            child: SizedBox(
+              width: 78,
+              height: 35,
+              child: TextButton(
+                style: TextButton.styleFrom(
                   backgroundColor: primaryPinkColor,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(4),
-                  )),
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const NavigationHomepage(),
                   ),
-                );
-              },
-              child: const Text(
-                'Contribute',
-                style: TextStyle(
-                  fontFamily: 'Bricolage-B',
-                  fontSize: 12.5,
-                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const NavigationHomepage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Contribute',
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontFamily: 'Bricolage-B',
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           )
         ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(1),
-          child: Container(
-            color: Colors.grey.shade300,
-            height: 1,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  controller: textController,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Say something',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Bricolage-R',
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ), // Remove the default underline
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async {
+                  if (uri != null && await canLaunchUrl(uri)) {
+                    await launchUrl(uri, mode: LaunchMode.externalApplication);
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Could not launch ${widget.url}'),
+                      ),
+                    );
+                  }
+                },
+                child: Card(
+                  color: Colors.white,
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: LinkPreview(
+                      linkStyle: const TextStyle(
+                        color: Colors.blue,
+                        fontFamily: 'Bricolage-R',
+                        decoration: TextDecoration.none,
+                        fontSize: 12.5,
+                      ),
+                      enableAnimation: true,
+                      onPreviewDataFetched: (data) {
+                        setState(() {
+                          _previewData = data;
+                        });
+                      },
+                      previewData: _previewData,
+                      text: uri.toString(),
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-      body: Column(
-        children: [],
       ),
     );
   }
