f8f7b0c3452df066a62ae43eee5894556626a433
sisshmue <sisshmue404@gmail.com>
2025-04-23 22:43:07 +0700

    modify spin wheel UI and Spin method

--- lib/screens/spin_wheel.dart
+++ lib/screens/spin_wheel.dart
@@ -28,7 +28,6 @@ class _SpinWheelState extends State<SpinWheel> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
       showInitialBottomSheet();
@@ -41,10 +40,96 @@ class _SpinWheelState extends State<SpinWheel> {
     super.dispose();
   }
 
+  void spinWheel() {
+    // Check if the bottom sheet is still open before popping
+    if (ModalRoute.of(context)?.isCurrent != true) {
+      Navigator.pop(context); // Close the bottom sheet if open
+    }
+    final randomIndex = Fortune.randomInt(0, rewards.length);
+    controller.add(randomIndex);
+    setState(() {
+      spinning = true;
+    });
+    Future.delayed(const Duration(seconds: 5), () {
+      final result = rewards[randomIndex];
+      showModalBottomSheet(
+        isDismissible: false,
+        enableDrag: false,
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+        ),
+        builder: (context) => Container(
+          padding: const EdgeInsets.all(24),
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                width: 50,
+                height: 5,
+                margin: const EdgeInsets.only(bottom: 20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              Image.asset(
+                'images/gift-dynamic-color.png',
+                width: 98,
+                height: 98,
+              ),
+              const Text(
+                "🎉 Woohoo! You've won !",
+                style: TextStyle(
+                  fontSize: 19.53,
+                  fontFamily: 'Bricolage-M',
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                result,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: primaryPinkColor,
+                  fontFamily: 'Walone-B',
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                onPressed: () => Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RewardsPage()),
+                  (route) => false,
+                ),
+                label: const Text(
+                  "Redeem the Reward",
+                  style: TextStyle(
+                    fontFamily: 'Bricolage-B',
+                    color: Colors.white,
+                    fontSize: 15.63,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: primaryPinkColor,
+                  minimumSize: const Size.fromHeight(50),
+                  textStyle: const TextStyle(fontSize: 18),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
   void showInitialBottomSheet() async {
-    final randomIndex = await showModalBottomSheet<int>(
-      isDismissible: false,
-      enableDrag: false,
+    await showModalBottomSheet<int>(
+      // isDismissible: false,
+      // enableDrag: false,
       context: context,
       isScrollControlled: true,
       constraints: BoxConstraints(
@@ -52,54 +137,9 @@ class _SpinWheelState extends State<SpinWheel> {
       ),
       builder: (context) => InitialBottomSheet(
         rewards: rewards,
+        onSpinPressed: spinWheel,
       ),
     );
-
-    if (randomIndex != null) {
-      controller.add(randomIndex);
-      setState(() {
-        spinning = true;
-      });
-      Future.delayed(const Duration(seconds: 5), () {
-        final result = rewards[randomIndex];
-        showModalBottomSheet(
-          isDismissible: false,
-          enableDrag: false,
-          context: context,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
-          ),
-          builder: (context) => Container(
-            padding: const EdgeInsets.all(24),
-            width: double.infinity,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  "🎉 Congratulations!",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "You won: $result",
-                  style: const TextStyle(fontSize: 18),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const RewardsPage()),
-                    (route) => false,
-                  ),
-                  child: const Text("Redeem the Reward"),
-                )
-              ],
-            ),
-          ),
-        );
-      });
-    }
   }
 
   @override
@@ -155,26 +195,82 @@ class _SpinWheelState extends State<SpinWheel> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
               Container(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
-                height: 400, // Add fixed height
-                child: FortuneWheel(
-                  animateFirst: false,
-                  selected: controller.stream,
-                  items: [
-                    for (var reward in rewards)
-                      FortuneItem(child: Text(reward)),
-                  ],
+                height: 400,
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                  ),
+                  child: FortuneWheel(
+                    animateFirst: false,
+                    selected: controller.stream,
+                    items: [
+                      for (int i = 0; i < rewards.length; i++)
+                        FortuneItem(
+                          child: Text(rewards[i]),
+                          style: FortuneItemStyle(
+                            color: i.isEven
+                                ? Colors.white
+                                : Colors.blue, // Alternating colors
+                            borderColor: Colors.grey[300]!, // Border color
+                            borderWidth: 1.0, // Border width
+                            textStyle: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: i.isEven
+                                  ? Colors.black
+                                  : Colors.white, // Text color contrast
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
                 ),
               ),
-              if (spinning == false)
+              if (spinning == false) ...[
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.3,
                 )
+              ] else ...[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                const Text(
+                  'Spinning...',
+                  style: TextStyle(
+                    fontFamily: 'Bricolage-B',
+                    fontSize: 24.41,
+                    color: Colors.white,
+                  ),
+                ),
+              ]
             ],
           ),
         ),
       ),
+      floatingActionButton: spinning
+          ? null
+          : Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: FloatingActionButton.extended(
+                onPressed: spinWheel,
+                backgroundColor: Colors.white,
+                label: const Text(
+                  "Spin wheel for 30 roses",
+                  style: TextStyle(
+                    fontFamily: 'Bricolage-B',
+                    color: primaryPinkColor,
+                    fontSize: 15.63,
+                  ),
+                ),
+                //icon: const Icon(Icons.casino, color: Colors.white),
+              ),
+            ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
@@ -183,17 +279,14 @@ class InitialBottomSheet extends StatelessWidget {
   const InitialBottomSheet({
     super.key,
     required this.rewards,
+    required this.onSpinPressed,
   });
 
   final List<String> rewards;
+  final VoidCallback onSpinPressed;
 
   @override
   Widget build(BuildContext context) {
-    void _spinAndClose() {
-      final randomIndex = Fortune.randomInt(0, rewards.length);
-      Navigator.pop(context, randomIndex); // Return the index to parent
-    }
-
     return Container(
       height: MediaQuery.of(context).size.height * 0.6,
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
@@ -236,11 +329,9 @@ class InitialBottomSheet extends StatelessWidget {
                 fontFamily: 'Bricolage-B',
                 color: primaryPinkColor),
           ),
-          const SizedBox(
-            height: 5,
-          ),
+          const SizedBox(height: 5),
           SizedBox(
-            height: MediaQuery.of(context).size.height * 0.23,
+            height: MediaQuery.of(context).size.height * 0.29,
             child: GridView.builder(
               itemCount: rewards.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -250,16 +341,6 @@ class InitialBottomSheet extends StatelessWidget {
                 childAspectRatio: 1.0,
               ),
               itemBuilder: (context, index) {
-                // return Card(
-                //   color: Colors.purple[50],
-                //   child: Center(
-                //     child: Text(
-                //       rewards[index],
-                //       textAlign: TextAlign.center,
-                //       style: const TextStyle(fontSize: 14),
-                //     ),
-                //   ),
-                // );
                 return Material(
                   elevation: 1,
                   child: Container(
@@ -301,10 +382,17 @@ class InitialBottomSheet extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           ElevatedButton.icon(
-            onPressed: _spinAndClose,
-            icon: const Icon(Icons.casino),
-            label: const Text("Spin the Wheel"),
+            onPressed: onSpinPressed,
+            label: const Text(
+              "Spin wheel for 30 roses",
+              style: TextStyle(
+                fontFamily: 'Bricolage-B',
+                color: Colors.white,
+                fontSize: 15.63,
+              ),
+            ),
             style: ElevatedButton.styleFrom(
+              backgroundColor: primaryPinkColor,
               minimumSize: const Size.fromHeight(50),
               textStyle: const TextStyle(fontSize: 18),
               shape: RoundedRectangleBorder(
