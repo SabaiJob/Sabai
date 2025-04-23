8d72737294869c3557c8143dea0248249e742d21
Han Min Thaw <hanminthaw15@gmail.com>
2025-04-23 18:05:11 +0700

    fix spin wheel page

--- lib/screens/spin_wheel.dart
+++ lib/screens/spin_wheel.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
-import 'package:sabai_app/screens/navigation_homepage.dart';
+import 'package:sabai_app/constants.dart';
+import 'package:sabai_app/screens/get_rewards.dart';
 
 class SpinWheel extends StatefulWidget {
   const SpinWheel({super.key});
@@ -15,12 +17,12 @@ class SpinWheel extends StatefulWidget {
 class _SpinWheelState extends State<SpinWheel> {
   final StreamController<int> controller = StreamController<int>();
   final List<String> rewards = [
-    '🍕 Free Pizza',
-    '🎁 Gift Box',
-    '💰 \$10 Cash',
-    '🧋 Bubble Tea',
-    '🛍️ Shopping Coupon',
-    '❌ Try Again',
+    'Samsung',
+    'Bag',
+    'Keychain',
+    'iPhone',
+    'iPad',
+    'MacBook',
   ];
   bool spinning = false;
 
@@ -87,7 +89,7 @@ class _SpinWheelState extends State<SpinWheel> {
                   onPressed: () => Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const NavigationHomepage()),
+                        builder: (context) => const RewardsPage()),
                     (route) => false,
                   ),
                   child: const Text("Redeem the Reward"),
@@ -103,27 +105,75 @@ class _SpinWheelState extends State<SpinWheel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
-        title: const Text('Spin to Win'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: 400, // Add fixed height
-            child: FortuneWheel(
-              animateFirst: false,
-              selected: controller.stream,
-              items: [
-                for (var reward in rewards) FortuneItem(child: Text(reward)),
+        backgroundColor: const Color(0xFFFF88C0),
+        title: const Text(
+          'Spin Wheel',
+          style: TextStyle(
+              fontFamily: 'Bricolage-M', fontSize: 19.53, color: Colors.white),
+        ),
+        actions: [
+          Container(
+            width: 50,
+            height: 30,
+            margin: const EdgeInsets.only(right: 15),
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xFFFFEBF6)),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.asset(
+                  'images/rose.png',
+                  scale: 2.5,
+                ),
+                const Text(
+                  '46',
+                  style: TextStyle(
+                    fontSize: 15.6,
+                    fontFamily: 'Bricolage-R',
+                    color: primaryPinkColor,
+                  ),
+                )
               ],
             ),
-          ),
-          if (spinning == false)
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.3,
-            )
+          )
         ],
+        iconTheme: const IconThemeData(color: Colors.white),
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFFF88C0), Color(0xFFFF3997)])),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                height: 400, // Add fixed height
+                child: FortuneWheel(
+                  animateFirst: false,
+                  selected: controller.stream,
+                  items: [
+                    for (var reward in rewards)
+                      FortuneItem(child: Text(reward)),
+                  ],
+                ),
+              ),
+              if (spinning == false)
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                )
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -145,8 +195,8 @@ class InitialBottomSheet extends StatelessWidget {
     }
 
     return Container(
-      height: MediaQuery.of(context).size.height * 0.5,
-      padding: const EdgeInsets.all(20),
+      height: MediaQuery.of(context).size.height * 0.6,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
@@ -164,12 +214,31 @@ class InitialBottomSheet extends StatelessWidget {
             ),
           ),
           const Text(
-            '🎉 Try Your Luck!',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            'Try Your Luck!',
+            style: TextStyle(
+                fontSize: 24.4,
+                fontFamily: 'Bricolage-B',
+                color: primaryPinkColor),
           ),
           const SizedBox(height: 10),
-          const Text('Give it a spin and see what you win.'),
-          const SizedBox(height: 20),
+          const Text(
+            'Give it a spin and see what you win.',
+            style: TextStyle(
+                fontFamily: 'Walone-B', fontSize: 14, color: Color(0xFF2B2F32)),
+          ),
+          const SizedBox(height: 5),
+          const Divider(),
+          const SizedBox(height: 5),
+          const Text(
+            'Prizes',
+            style: TextStyle(
+                fontSize: 19.5,
+                fontFamily: 'Bricolage-B',
+                color: primaryPinkColor),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.23,
             child: GridView.builder(
@@ -181,13 +250,49 @@ class InitialBottomSheet extends StatelessWidget {
                 childAspectRatio: 1.0,
               ),
               itemBuilder: (context, index) {
-                return Card(
-                  color: Colors.purple[50],
-                  child: Center(
-                    child: Text(
-                      rewards[index],
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(fontSize: 14),
+                // return Card(
+                //   color: Colors.purple[50],
+                //   child: Center(
+                //     child: Text(
+                //       rewards[index],
+                //       textAlign: TextAlign.center,
+                //       style: const TextStyle(fontSize: 14),
+                //     ),
+                //   ),
+                // );
+                return Material(
+                  elevation: 1,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: RadialGradient(
+                        colors: [
+                          Color(0xFFFFDDE6),
+                          Color(0xFFFFB8CA),
+                        ],
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          width: 84,
+                          height: 88,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 5),
+                          child: Text(
+                            overflow: TextOverflow.ellipsis,
+                            rewards[index],
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontFamily: 'Bricolage-M', fontSize: 10),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 );
